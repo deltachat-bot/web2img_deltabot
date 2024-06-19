03007659b8ddd9d3e6347310eea515073bbf83b4
adbenitez <asieldbenitez@gmail.com>
2024-06-19 23:33:55 +0200

    fix linter warning

--- web2img_deltabot/hooks.py
+++ web2img_deltabot/hooks.py
@@ -30,7 +30,10 @@ cli.add_generic_option(
     help="do not display date timestamp in log messages",
     action="store_false",
 )
-HELP = "📸 I am a Delta Chat bot, send me any website URL to save it as image. Example: https://delta.chat"
+HELP = (
+    "📸 I am a Delta Chat bot, send me any website URL to save it as image."
+    " Example: https://delta.chat"
+)
 
 
 @cli.on_init
